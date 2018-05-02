0ec2f5765437fb55dbfcdad34918aa9176b84907
Kamesh <kamesh231@gmail.com>
2018-05-02 14:28:01 +0530

    First Project Setup

---
+++ BleedRedAzure/src/main/java/org/bleedred/app/BleedRedAzureApplication.java
@@ -0,0 +1,12 @@
+package org.bleedred.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BleedRedAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BleedRedAzureApplication.class, args);
+	}
+}
